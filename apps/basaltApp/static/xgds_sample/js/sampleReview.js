7e61cfb2e2572489b98ee2146ec1b0f94d0d40b2
Grace Lee <yeongrace.lee@gmail.com>
2016-02-25 05:02:07 +0000

    added field to forms

--- apps/basaltApp/static/xgds_sample/js/sampleReview.js
+++ apps/basaltApp/static/xgds_sample/js/sampleReview.js
@@ -5,3 +5,12 @@ function constructSampleView(data) {
 	var sampleViewTemplate = $(newDiv);
 	var newEl = $container.append(imageViewTemplate);
 }
+
+//datetime picker
+var dateTimeOptions = {'controlType': 'select',
+	  	       'oneLine': true,
+	  	       'showTimezone': false,
+	  	       'timezone': '-0000'
+	 	       };
+
+$("#id_collection_time").datetimepicker(dateTimeOptions);
