ef223a94894aaf6d6968529eb15b5ad4f4f107c2
Grace Lee <yeongrace.lee@gmail.com>
2016-03-07 23:49:58 +0000

    working on sample search

--- apps/basaltApp/defaultSettings.py
+++ apps/basaltApp/defaultSettings.py
@@ -38,4 +38,4 @@ import os
 
 basaltApp_BOWER_INSTALLED_APPS = ()
 XGDS_SAMPLE_START_YEAR = 2016
-XGDS_SAMPLE_HANDLEBARS_DIR = [os.path.join('basaltApp', 'templates', 'handlebars')]
+XGDS_SAMPLE_HANDLEBARS_DIR = [os.path.join('basaltApp', 'templates', 'xgds_sample')]
