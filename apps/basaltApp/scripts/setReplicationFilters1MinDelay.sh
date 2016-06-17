fb39f9b1329856279816a27e38bd2d31a08816c6
Grace Lee <grace.lee@nasa.gov>
2016-06-17 07:31:13 -0700

    Add scripts to control replication filters and delays

---
+++ apps/basaltApp/scripts/setReplicationFilters1MinDelay.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+/home/irg/tungsten/tungsten/tools/tpm update --hosts=boat,shore --repl-svc-applier-filters=delay,replicate --property=replicator.filter.delay.delay=60 --property=replicator.filter.replicate.ignore=xgds_basalt.xgds_video_videosegment,xgds_basalt.xgds_video_videoepisode,xgds_basalt.xgds_core_constant
+
+ssh boat ./tungsten/tungsten/tools/tpm update --hosts=boat,shore --repl-svc-applier-filters=delay,replicate --property=replicator.filter.delay.delay=60 --property=replicator.filter.replicate.ignore=xgds_basalt.xgds_video_videosegment,xgds_basalt.xgds_video_videoepisode,xgds_basalt.xgds_core_constant
