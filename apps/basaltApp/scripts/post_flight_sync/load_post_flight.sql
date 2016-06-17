02e6e4b1ad032fdceb5a3510775c591294312e11
Tamar Cohen <tamar.cohen@nasa.gov>
2016-06-17 12:38:57 -0600

    post flight sync scripts ready to test

--- apps/basaltApp/scripts/post_flight_sync/load_post_flight.sql
+++ apps/basaltApp/scripts/post_flight_sync/load_post_flight.sql
@@ -1,11 +1,11 @@
+# only load data from boat that is in /tmp/boat
 set FOREIGN_KEY_CHECKS=0;
 set @today=DATE(NOW());
-set @prefix='/tmp/boat/xgds_video_video';
 
-delete from xgds_video_videoepisode;
-delete from xgds_video_videosegment;
+delete from xgds_video_videoepisode where startTime>@today;
+delete from xgds_video_videosegment where startTime>@today;
 
-load data infile '/tmp/boat/xgds_video_videosegment.sql' into table xgds_video_videosegment;
-load data infile '/tmp/boat/xgds_video_videoepisode.sql' into table xgds_video_videoepisode;
+load data infile '/tmp/boat/boat/boat_video_segment.sql' into table xgds_video_videosegment;
+load data infile '/tmp/boat/boat/boat_video_episode.sql' into table xgds_video_videoepisode;
 
 set FOREIGN_KEY_CHECKS=1;
