e3dedfdad741718b424da44e27131a508624e9c8
Tamar Cohen <tamar.cohen@nasa.gov>
2016-11-02 18:43:49 -0700

    work in progress selection on map, notes and images.  needs more testing

--- apps/basaltApp/static/xgds_notes2/js/olNoteMap.js
+++ apps/basaltApp/static/xgds_notes2/js/olNoteMap.js
@@ -1,24 +1,40 @@
 // render json note information on the openlayers map
 
 var Note = {
+		selectedStylePath: '/static/xgds_notes2/icons/post_office_selected.png',
+		stylePath: '/static/xgds_notes2/icons/post_office.png',
         initStyles: function() {
             if (_.isUndefined(this.styles)){
                 this.styles = {};
                 this.styles['iconStyle'] = new ol.style.Style({
+                	zIndex: 1,
                     image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
-                        src: '/static/xgds_notes2/icons/post_office.png',
+                        src: this.stylePath,
                         scale: 0.25
                         }))
                       });
+                this.styles['selectedIconStyle'] = new ol.style.Style({
+                	zIndex: 10,
+                    image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
+                        src: this.selectedStylePath,
+                        scale: 0.25
+                        }))
+                      });
+                this.styles['yellowStroke'] =  new ol.style.Stroke({
+                    color: 'yellow',
+                    width: 2
+                });
+                this.styles['greenStroke'] =  new ol.style.Stroke({
+                    color: 'green',
+                    width: 2
+                });
                 this.styles['text'] = {
+                	zIndex: 1,
                     font: '12px Calibri,sans-serif',
                     fill: new ol.style.Fill({
                         color: 'black'
                     }),
-                    stroke: new ol.style.Stroke({
-                        color: 'yellow',
-                        width: 2
-                    }),
+                    stroke: this.styles['yellowStroke'],
                     offsetY: -15
                 };
             }
@@ -36,7 +52,7 @@ var Note = {
                 }
             }
             var vectorLayer = new ol.layer.Vector({
-                name: "Notes",
+                name: notesJson[0].type,
                 source: new ol.source.Vector({
                     features: olFeatures
                 }),
@@ -47,8 +63,11 @@ var Note = {
         	noteJson.flattenedTags = '';
             var coords = transform([noteJson.lon, noteJson.lat]);
             var feature = new ol.Feature({
+            	selected: false,
                 name: getLocalTimeString(noteJson.event_time, noteJson.event_timezone),
                 uuid: noteJson.pk,
+                pk: noteJson.pk,
+                type: noteJson.type,
                 geometry: new ol.geom.Point(coords)
             });
             feature.setStyle(this.getStyles(noteJson));
@@ -71,6 +90,26 @@ var Note = {
                 styles.push(textStyle);
             }
             return styles;
+        },
+        selectMapElement:function(feature){
+        	feature.selected = false;
+        	var styles = feature.getStyle();
+        	var newstyles = [this.styles['selectedIconStyle']];
+        	var newtextstyle = styles[1];
+        	newtextstyle.getText().setStroke(this.styles['greenStroke']);
+        	newtextstyle.setZIndex(10);
+        	newstyles.push(newtextstyle);
+        	feature.setStyle(newstyles);
+        },
+        deselectMapElement:function(feature){
+        	feature.selected = true;
+        	var styles = feature.getStyle();
+        	var newstyles = [this.styles['iconStyle']];
+        	var newtextstyle = styles[1];
+        	newtextstyle.getText().setStroke(this.styles['yellowStroke']);
+        	newtextstyle.setZIndex(1);
+        	newstyles.push(newtextstyle);
+        	feature.setStyle(newstyles);
         },
         setupPopup: function(feature, noteJson) {
             var trString = "<tr><td>%s</td><td>%s</td></tr>";
