39bbf2c054f35c9b1a4b3466a5aa9e1e3687806d
Grace Lee <yeongrace.lee@gmail.com>
2016-11-05 03:51:44 +0000

    bug fix in replicator script

--- apps/basaltApp/scripts/writePolygonsToFile.sql
+++ apps/basaltApp/scripts/writePolygonsToFile.sql
@@ -1,11 +1,11 @@
 select uuid, name, description, visible, popup, showLabel, image, height, width, polygon, labelStyle_id, mapLayer_id, style_id 
 into outfile '/tmp/tempSql/groundoverlay.sql' from xgds_map_server_groundoverlay;
 
-select uuid, name, description, visible, popup, showLabel, polygon, labelStyle_id, maplayer_id, style_id 
+select uuid, name, description, visible, popup, showLabel, polygon, labelStyle_id, mapLayer_id, style_id 
 into outfile '/tmp/tempSql/polygon.sql' from xgds_map_server_polygon;
 
-select uuid, name, description, visible, popup, showLabel, point, labelStyle_id, maplayer_id, style_id 
+select uuid, name, description, visible, popup, showLabel, point, icon_id, labelStyle_id, mapLayer_id, style_id 
 into outfile '/tmp/tempSql/point.sql' from xgds_map_server_point;
 
-select uuid, name, description, visible, popup, showLabel, linestring, labelStyle_id, maplayer_id, style_id 
+select uuid, name, description, visible, popup, showLabel, lineString, labelStyle_id, maplayer_id, style_id 
 into outfile '/tmp/tempSql/linestring.sql' from xgds_map_server_linestring;
