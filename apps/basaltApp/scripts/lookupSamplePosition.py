e459641499d89fe456b7e1433e735d2f5beb98ec
Grace Lee <yeongrace.lee@gmail.com>
2016-11-09 01:58:25 +0000

    modified lookup sample position

--- apps/basaltApp/scripts/lookupSamplePosition.py
+++ apps/basaltApp/scripts/lookupSamplePosition.py
@@ -8,24 +8,37 @@ from basaltApp.models import *
 from geocamTrack.utils import getClosestPosition
 
 missingPositionSampleList = []
-startTime = datetime.datetime(2016,6,14,0,6,0,tzinfo=pytz.utc)
+startTime = datetime.datetime(2016,10,30,0,6,0,tzinfo=pytz.utc)
 samples = BasaltSample.objects.filter(collection_time__gte = startTime).order_by("collection_time")
 sampleCount = 0
 missingPositionCount = 0
 for sample in samples:
-    if sample.resource is None:
-        print "**** Processing: %s ****" % sample.name
-        sample.resource = BasaltResource.objects.get(name=settings.XGDS_SAMPLE_DEFAULT_COLLECTOR)
-        sample.track_position = getClosestPosition(timestamp=sample.collection_time, resource=sample.resource)
-        if not sample.track_position:
-            sample.track_position = getClosestPosition(timestamp=sample.collection_time, resource=BasaltResource.objects.get(name="EV1"))
-        if not sample.track_position:
-            print "  No location found - adding to list of samples w/o position"
-            missingPositionSampleList.append(sample.name)
-            missingPositionCount += 1
-        print "  Date: %s, Resource: %s, Location: %s\n" % (sample.collection_time, sample.resource, sample.track_position)
-        sample.save()
-        sampleCount += 1
+    if (not sample.track_position) and (not sample.user_position):
+        if sample.resource:
+            print "**** Processing: %s ****" % sample.name
+            sample.track_position = getClosestPosition(timestamp=sample.collection_time, resource=sample.resource)
+            if not sample.track_position:
+                sample.track_position = getClosestPosition(timestamp=sample.collection_time, resource=sample.resource)
+            if not sample.track_position:
+                print "  No location found - adding to list of samples w/o position"
+                missingPositionSampleList.append(sample.name)
+                missingPositionCount += 1
+            print "  Date: %s, Resource: %s, Location: %s\n" % (sample.collection_time, sample.resource, sample.track_position)
+            sample.save()
+            sampleCount += 1
+#     if sample.resource is None:
+#         print "**** Processing: %s ****" % sample.name
+#         sample.resource = BasaltResource.objects.get(name=settings.XGDS_SAMPLE_DEFAULT_COLLECTOR)
+#         sample.track_position = getClosestPosition(timestamp=sample.collection_time, resource=sample.resource)
+#         if not sample.track_position:
+#             sample.track_position = getClosestPosition(timestamp=sample.collection_time, resource=BasaltResource.objects.get(name="EV1"))
+#         if not sample.track_position:
+#             print "  No location found - adding to list of samples w/o position"
+#             missingPositionSampleList.append(sample.name)
+#             missingPositionCount += 1
+#         print "  Date: %s, Resource: %s, Location: %s\n" % (sample.collection_time, sample.resource, sample.track_position)
+#         sample.save()
+#         sampleCount += 1
 
 print "Processed %d samples. %d of them were missing position info" % (sampleCount, missingPositionCount)
 f = open("SamplesMissingPositions.txt", "w")
