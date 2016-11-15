389370f18a7e7632a10e5599358291ad0c6d7c20
Tamar Cohen <tamar.cohen@nasa.gov>
2016-11-15 12:08:48 -1000

    updated hvnp kml to work from command line

--- apps/basaltApp/hvnp_air_quality/hvnp_kml_generator.py
+++ apps/basaltApp/hvnp_air_quality/hvnp_kml_generator.py
@@ -18,7 +18,6 @@
 
 # This Python script will generate a KML network link and a KML file which renders 
 # placemarks for the air quality at Hawai'i Volcanoes National Park.
-
 import datetime
 import pytz
 import requests
@@ -47,9 +46,24 @@ CONDITIONS = {}
 SITE_POSITIONS = {'Visitor Center': {'lat': 19.4308,
                                      'lon': -155.2578,
                                      'alt': 1215},
-                    'Jaggar Museum': {'lat': 19.4203,
+                  'Jaggar Museum': {'lat': 19.4203,
                                     'lon': -155.2881,
-                                    'alt': 1123}}
+                                    'alt': 1123},
+                  'Campground': {'lat': 19.42624166666667,
+                                 'lon': -155.29497777777777},
+                  'Steam Vents': {'lat': 19.431455555555555,
+                                  'lon': -155.26766944444446},
+                  'Parking Lot': {'lat': 19.429644444444445,
+                                  'lon': -155.25747777777778},
+                  'Thurston': {'lat':19.41423611111111,
+                               'lon': -155.23893055555556},
+                  'Devastation': {'lat': 19.406405555555555,
+                                  'lon': -155.25293333333335},
+                  'Kealakomo': {'lat':19.316969444444446,
+                                'lon': -155.16187222222223},
+                  'End of Coastal Road': {'lat':19.292941666666668,
+                                          'lon': -155.10755833333332}
+                  }
 
 WEDGE_POSITIONS = {"Pu'U O'o Crater": {'lat': 19.387979,
                                        'lon': -155.107504,
@@ -73,6 +87,8 @@ WEDGE_LENGTH = 50.0 # in km
 WEDGE_ANGLE = 30 # in degrees
 CIRCLE_RADIUS = 2.0 #in km
 DEGREE_SYMBOL = unichr(176)
+
+NETWORK_LINK_NAME = 'HVNP_SO2' # The name that will show up in Google Earth for the network link
 
 def getCurrentStateKml(serverUrl):
     ''' Create the overall KML for the current state of the air quality.
@@ -117,7 +133,7 @@ def getCurrentStateKml(serverUrl):
     return document.to_string(prettyprint=True)
 
 
-def buildNetworkLink(url, name, interval=300):
+def buildNetworkLinkContents(url, name=NETWORK_LINK_NAME, interval=300):
     ''' Build a Network link to refresh an url every interval seconds
         Right now we are defaulting to 5 minute refreshes as the user may not have opened this close to a quarter hour.
     '''
@@ -133,6 +149,21 @@ def buildNetworkLink(url, name, interval=300):
 ''' % dict(name=escape(name),
            interval=interval,
            url=escape(url))
+
+
+def buildNetworkLinkKMLFile(url, name=NETWORK_LINK_NAME, interval=300):
+    ''' Build a full KML file containing the network link.
+    '''
+    return \
+'''<?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://earth.google.com/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">
+
+  <Document id="%(name)s">
+    <name>%(name)s</name>
+    %(text)s
+  </Document>
+</kml>
+''' % dict(name=escape(name), text=buildNetworkLinkContents(url, name, interval))
 
 
 def getRawData():
@@ -320,10 +351,18 @@ def buildPlacemarkForSite(folder, siteData):
 def getSiteDescription(siteData):
     ''' Get printable string to show when the user clicks on a placemark for a site '''
     result = ('%s\n') % getDataValue('name', siteData)
-    if 'SO2' in siteData:
-        result += ('SO2: %f ppm\n') % float(getDataValue('SO2.average15', siteData))
-    if 'PM25' in siteData:
-        result += ('PM25: %d %sg/m3\n') % (int(getDataValue('PM25.average15', siteData)), unichr(956))
+    try:
+        if 'SO2' in siteData:
+            so2Value = getDataValue('SO2.average15', siteData)
+            result += ('SO2: %f ppm\n') % float(so2Value)
+    except:
+        pass
+    try:
+        if 'PM25' in siteData:
+            pm25Value = int(getDataValue('PM25.average15', siteData))
+            result += ('PM25: %d %sg/m3\n') % (pm25Value, unichr(956))
+    except:
+        pass
     return result
 
 
@@ -352,7 +391,10 @@ def getCraterDescription(siteData):
 
 def buildPoint(location, name):
     pointID = makeKey(name) + '_point'
-    pointShape =  Point([(location['lon'], location['lat'], location['alt'])])
+    alt = 0.0
+    if alt in location:
+        alt = location['alt']
+    pointShape =  Point([(location['lon'], location['lat'], alt)])
     kmlGeometry = geometry.Geometry(NAME_SPACE, id=pointID, geometry=pointShape, extrude=False, tessellate=False, altitude_mode='clampToGround')
     return kmlGeometry
 
@@ -388,3 +430,36 @@ def buildCircle(location, name):
     circleShape = Polygon(coords) 
     kmlGeometry = geometry.Geometry(NAME_SPACE, id=circleID, geometry=circleShape, extrude=False, tessellate=True, altitude_mode='clampToGround')
     return kmlGeometry
+
+
+def main():
+    import optparse
+    parser = optparse.OptionParser('usage: %prog --url myserver/hvnp_so2.kml' + 
+                                   '--outputDir <dirname> ' +
+                                   '--recorderId <recorderID>')
+    parser.add_option('-c', '--childKml', dest='childKml', action='store_true',
+                      help='True to generate only the child kml with data')
+    parser.add_option('-p', '--prefix', dest="prefix", default=SERVER_URL,
+                      help='Prefix for server url; Google Earth will hit this server (ie http://hawaiiso2network.com).')
+    parser.add_option('-i', '--interval', dest="interval", default=300,
+                      help='Refresh interval for network link in seconds')
+    parser.add_option('-u', '--url', dest="url",
+                      help='Url on your server that will hit this script and generate the child kml')
+    
+    opts, args = parser.parse_args()
+    if len(args) != 0:
+        parser.error('expected no arguments')
+    if not (opts.prefix):
+        parser.error("Prefix option is required.")
+
+    if opts.childKml:
+        result = getCurrentStateKml(opts.prefix).encode('utf-8');
+        print result
+        return result
+    else:
+        result = buildNetworkLinkKMLFile(opts.prefix + '/' + opts.url, interval=opts.interval).encode('utf-8')
+        print result
+        return result
+
+if __name__ == '__main__':
+    main()
