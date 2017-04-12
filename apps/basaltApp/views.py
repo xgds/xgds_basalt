7a2328d642ae7186b274af1306e50f9d7b7de603
David Lees <david.lees@nasa.gov>
2017-04-12 15:58:53 -0700

    Add more users (pxrf, flir, asd and EVs) to initial fixtures. Clean up some trailing blanks at ends of lines.

--- apps/basaltApp/views.py
+++ apps/basaltApp/views.py
@@ -552,7 +552,7 @@ def saveNewPxrfData(request, jsonResult=False):
                                object_id=object_id)
             if result['status'] == 'success':
                 # relay if needed
-                if 'relay' in request.POST:                                                                                                                                   
+                if 'relay' in request.POST:
                     theModel = getModelByName(settings.XGDS_MAP_SERVER_JS_MAP[result['modelName']]['model'])
                     theInstance = theModel.objects.get(pk=result['pk'])
                     addRelayFiles(theInstance, request.FILES, json.dumps(request.POST), request.get_full_path())
