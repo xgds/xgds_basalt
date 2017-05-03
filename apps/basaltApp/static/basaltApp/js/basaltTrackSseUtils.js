15d800778189013495cf7d112779edf96a451383
Tamar Cohen <tamar.cohen@nasa.gov>
2017-05-02 17:43:12 -0700

    preload icons because Chrome is a POS

--- apps/basaltApp/static/basaltApp/js/basaltTrackSseUtils.js
+++ apps/basaltApp/static/basaltApp/js/basaltTrackSseUtils.js
@@ -19,16 +19,32 @@ $.extend(trackSse, {
 	getTrackModel: function() {
 		return app.options.searchModels['Actual_Traverse'].model;
 	},
+	lookupImage: function(url){
+		var result = undefined;
+		$.each(trackSse.preloadedIcons, function(index, theImg){
+			if (theImg.src == url){
+				result = theImg;
+			}
+		});
+		return result;
+		
+	},
 	setupPositionIcon: function(channel){
 		Position.initStyles();
 		if (!(channel in Position.styles)){
 			var pointerPath = '/static/basaltApp/icons/' + channel.toLowerCase() + '_pointer.png';
-			Position.styles[channel] = new ol.style.Style({
-                image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
-                    src: pointerPath,
-                    scale: 0.5
-                    }))
+			var theImg = trackSse.lookupImage(pointerPath);
+			// these were preloaded in MapView.html
+			var theIcon = new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
+                src: pointerPath,
+                img: theImg,  // this is for preload to fix Chrome.
+                scale: 0.5
+                }));
+
+			var newStyle = new ol.style.Style({
+                image: theIcon
             });
+			Position.styles[channel] = newStyle;
 		}
 	}
 });
