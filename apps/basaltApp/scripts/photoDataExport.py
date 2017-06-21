e7eb0697f9362cc5ec5b8a21b064873eda6ed329
David Lees <david.lees@nasa.gov>
2017-06-21 13:40:57 -0700

    Add postion lookup stuff to script.  Use acquistion_time for timestamp

--- apps/basaltApp/scripts/photoDataExport.py
+++ apps/basaltApp/scripts/photoDataExport.py
@@ -4,23 +4,31 @@ import django
 from datetime import datetime
 import pytz
 django.setup()
-from basaltApp.models import BasaltImageSet, BasaltSingleImage
+from basaltApp.models import BasaltImageSet, BasaltSingleImage, BasaltResource
 from geocamTrack.utils import getClosestPosition
 
 hawaiiStandardTime = pytz.timezone('US/Hawaii')
 startTime = datetime(2016, 11, 8, 0, 0, 0, tzinfo=hawaiiStandardTime)
 endTime = datetime(2016, 11, 9, 0, 0, 0, tzinfo=hawaiiStandardTime)
+ev1Resource = BasaltResource.objects.get(name="EV1")
+print "Resource:", ev1Resource
 
-imgList = BasaltImageSet.objects.filter(creation_time__gte=startTime).filter(creation_time__lte=endTime)
+imgList = BasaltImageSet.objects.filter(acquisition_time__gte=startTime).filter(acquisition_time__lte=endTime)
 
 print "Found %d images." % imgList.count()
 
 for img in imgList:
+    position = getClosestPosition(resource=ev1Resource, timestamp=img.acquisition_time)
     if img.flight:
-        print img.flight.name
+        print "F: %s, N: %s, P: %s" % (img.flight.name, img.name, position)
         singleImages = img.images
         print "  Images:"
         for si in singleImages.all():
             print "    Thumb: %s (%s x %s)" % (si.thumbnail, si.width, si.height)
     else:
-        print "<none>"
+        position = getClosestPosition(resource=ev1Resource, timestamp=img.acquisition_time)
+        print "F: %s, N: %s, P: %s" % ("<none>", img.name, position)
+        singleImages = img.images
+        print "  Images:"
+        for si in singleImages.all():
+            print "    Thumb: %s (%s x %s)" % (si.thumbnail, si.width, si.height)
