ca15bc99949299a187990450a74f9460807f8e22
David Lees <david.lees@nasa.gov>
2017-07-23 13:21:30 -0700

    new script to backup and restore tungsten properties

---
+++ apps/basaltApp/scripts/backupOriginalServiceProperties.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# Saves a copy of the unmodified tungsten replicator service properties files
+# so the tpm command does not die.  It does not play well with customized
+# property file (e.g. local javascript filters).
+
+rsync -auv ~/tungsten/releases/tungsten-replicator-oss-4.0.1-123_pid[0-9]*/tungsten-replicator/conf/static-[A-Z,a-z]*.properties ~/replicator-properties/orig
+x
