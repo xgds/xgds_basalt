9948e176b36aadf7e35e9f85da2f28ef04a29b95
xGDS Staff <xGDS@xgds.org>
2017-11-03 18:20:49 -0700

    Fix ansible playbook for code checkout

--- scripts/ansible/update-code.sh
+++ scripts/ansible/update-code.sh
@@ -1,1 +1,1 @@
-ansible-playbook update-code.yml --inventory-file="localhost, shore, bpc1, bpc2, bpc3, old-shore" -K
+ansible-playbook update-code.yml --inventory-file="boat, shore, bpc1, bpc2, bpc3, old-shore" -K
