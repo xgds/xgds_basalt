803c665a59df37fbf251562f47f37892a5e8d66f
Victor Woeltjen <victor.woeltjen@nasa.gov>
2017-12-21 18:27:15 +0000

    sketch in dependency limitation
    ...only want to collect static files from dependencies in use.

--- apps/django_npm/finders.py
+++ apps/django_npm/finders.py
@@ -14,7 +14,10 @@
 # specific language governing permissions and limitations under the License.
 # __END_LICENSE__
 
+import os
+import json
 from django.contrib.staticfiles.finders import AppDirectoriesFinder
+
 
 class NpmAppFinder(AppDirectoriesFinder):
     """
@@ -22,3 +25,35 @@ class NpmAppFinder(AppDirectoriesFinder):
     specified in the source_dir attribute.
     """
     source_dir = 'node_modules'
+    dependency_sets = {}
+
+    def load_dependencies(self, storage):
+        if not os.path.basename(storage.location) == self.source_dir:
+            return set()
+
+        package_dir = os.path.dirname(storage.location)
+        package_path = os.path.join(package_dir, 'package.json')
+
+        if not os.path.exists(package_path):
+            return set()
+
+        package = json.load(open(package_path))
+
+        if not 'dependencies' in package:
+            return set()
+
+        return set(package['dependencies'].keys())
+
+    def is_dependency(self, path, storage):
+        if not storage.location in self.dependency_sets:
+            self.dependency_sets[storage.location] = self.load_dependencies(storage)
+        module_name = path.split(os.sep)[0]
+        return module_name in self.dependency_sets[storage.location]
+
+    def list(self, ignore_patterns):
+        """
+        List all files in all app storages.
+        """
+        for path, storage in AppDirectoriesFinder.list(self, ignore_patterns):
+            if self.is_dependency(path, storage):
+                yield path, storage
